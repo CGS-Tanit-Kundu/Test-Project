e510a99300d7cf9139a06db91137bd5a48f62407
Tanit Kundu <Tanit.kundu@cgs.act.edu.au>
2015-07-23 11:41:18 +1000

    Initial commit

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -11,13 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        println("Hello IT1002");
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
 
